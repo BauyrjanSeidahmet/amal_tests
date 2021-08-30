1a02a9c7a4130160460cdbb9bb9d8d9eb0554240
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-30 13:55:36 +0600

    редактировал функции согласно нашего приложения

--- step_definitions/steps.js
+++ step_definitions/steps.js
@@ -1,27 +1,18 @@
 const { I } = inject();
-// Add in your custom step files
 
-Given(/^я перехожу на страницу "(.*?)"$/, () => {
-  // From "features/login.feature" {"line":6,"column":1}
-  throw new Error("Not implemented yet");
+Given("я перехожу на страницу {string}", (page) => {
+  I.amOnPage(page);
 });
 
-When("я ввожу {string} в поле {string}", () => {
-  // From "features/login.feature" {"line":7,"column":1}
-  throw new Error("Not implemented yet");
+When("я ввожу {string} в поле {string}", (text, fieldName) => {
+  I.fillField({ name: fieldName }, text);
 });
 
-When("я ввожу {string} в поле {string}", () => {
-  // From "features/login.feature" {"line":8,"column":1}
-  throw new Error("Not implemented yet");
+When("я нажимаю на кнопку {string}", (buttonId) => {
+  I.click({ id: buttonId });
 });
 
-When("я нажимаю на кнопку {string}", () => {
-  // From "features/login.feature" {"line":9,"column":1}
-  throw new Error("Not implemented yet");
-});
-
-Then(/^я перехожу на главную страницу "(.*?)"$/, () => {
-  // From "features/login.feature" {"line":10,"column":1}
-  throw new Error("Not implemented yet");
+Then("я перехожу на главную страницу {string}", (page) => {
+  I.wait(2);
+  I.amOnPage(page);
 });
