70da9b8cf7bab5cea33629085eba5f65c7715772
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-30 12:14:22 +0600

    созданы функции в step.js

--- step_definitions/steps.js
+++ step_definitions/steps.js
@@ -1,6 +1,27 @@
 const { I } = inject();
 // Add in your custom step files
 
-Given("I have a defined step", () => {
-  // TODO: replace with your own step
+Given(/^я перехожу на страницу "(.*?)"$/, () => {
+  // From "features/login.feature" {"line":6,"column":1}
+  throw new Error("Not implemented yet");
+});
+
+When("я ввожу {string} в поле {string}", () => {
+  // From "features/login.feature" {"line":7,"column":1}
+  throw new Error("Not implemented yet");
+});
+
+When("я ввожу {string} в поле {string}", () => {
+  // From "features/login.feature" {"line":8,"column":1}
+  throw new Error("Not implemented yet");
+});
+
+When("я нажимаю на кнопку {string}", () => {
+  // From "features/login.feature" {"line":9,"column":1}
+  throw new Error("Not implemented yet");
+});
+
+Then(/^я перехожу на главную страницу "(.*?)"$/, () => {
+  // From "features/login.feature" {"line":10,"column":1}
+  throw new Error("Not implemented yet");
 });
